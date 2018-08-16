c47fdaaa11a5534d2b06f323031831a95a9a335e
abhinav272 <abhinav.ih@gmail.com>
2018-08-16 16:37:24 +0530

    exif info added

--- app/src/main/java/com/abhinav/instacropperpicker/SizeUtils.java
+++ app/src/main/java/com/abhinav/instacropperpicker/SizeUtils.java
@@ -3,6 +3,7 @@ package com.abhinav.instacropperpicker;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 
+import com.yalantis.ucrop.util.BitmapLoadUtils;
 import com.yalantis.ucrop.util.FileUtils;
 
 public class SizeUtils {
@@ -10,6 +11,7 @@ public class SizeUtils {
     private Uri fileUri;
     private float height;
     private float width;
+    private int exifOrientation;
 
     public SizeUtils(Uri fileUri) {
         this.fileUri = fileUri;
@@ -27,13 +29,26 @@ public class SizeUtils {
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeFile(FileUtils.getPath(App.getContext(), fileUri), options);
+        exifOrientation = BitmapLoadUtils.exifToDegrees(BitmapLoadUtils.getExifOrientation(App.getContext(), fileUri));
         width = options.outWidth;
         height = options.outHeight;
     }
 
     public float getAspectRatio() {
+        float ratio = 1f;
         if (height == 0 || width == 0)
             init();
-        return width / height;
+        switch (exifOrientation) {
+            case 0:
+            case 180:
+                ratio = width / height;
+                break;
+            case 90:
+            case 270:
+                ratio = height / width;
+                break;
+        }
+        return ratio;
+
     }
 }
